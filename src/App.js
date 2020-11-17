750d551fa611a68f7c8d15b93894c7c0902ca05c
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2020-11-17 12:06:12 +0100

    Add Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="container">
+			<Router>
+				<Header />
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
