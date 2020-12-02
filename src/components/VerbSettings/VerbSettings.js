e30f7252eccc2b37c0ef421234d7cac529628d4c
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2020-12-02 19:20:18 +0100

    Improve form validation, and set up the game based on the user's preferences

--- src/components/VerbSettings/VerbSettings.js
+++ src/components/VerbSettings/VerbSettings.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
@@ -7,7 +7,13 @@ import {
 	setReflexiveVerbs,
 	setSelectedVerbs,
 	setUserDefinedVerbs,
+	setQuestionList,
+	toggleCurrentlyPlaying,
+	setCurrentQuestion,
+	setTargetScore,
+	setErrors,
 } from '../../actions';
+import generateQuestionList from '../../utils/generateQuestionList';
 import PronounItem from '../PronounItem/PronounItem';
 import './VerbSettings.css';
 
@@ -15,21 +21,91 @@ export default function VerbSettings() {
 	const tenses = useSelector((state) => state.tenses);
 	const pronouns = useSelector((state) => state.pronouns);
 	const verbSettings = useSelector((state) => state.verbSettings);
-	const tenseError = tenses.every((tenseObj) => !tenseObj.selected);
-	const pronounsError = pronouns.every((pronounObj) => !pronounObj.selected);
-	const verbsError =
-		verbSettings.selectedVerbs === 'USER_DEFINED' &&
-		verbSettings.userDefinedVerbs === '';
+	const targetScore = useSelector((state) => state.targetScore);
+	const errors = useSelector((state) => state.errors);
 
 	const dispatch = useDispatch();
+	const history = useHistory();
+
+	function handleSubmitClick(){
+		if (validateForm()) {
+			setUpGame();
+		}
+	}
+	
+	const validateForm = () => {
+		let formErrors = {};
+		let formIsValid = true;
+
+		// Check that at least one tense is selected.
+		if (tenses.every((tense) => !tense.selected)) {
+			formIsValid = false;
+			formErrors['tenses'] = 'Please select at least one tense.';
+		}
+
+		// Check that at least one pronoun is selected.
+		if (pronouns.every((pronoun) => !pronoun.selected)) {
+			formIsValid = false;
+			formErrors['pronouns'] = 'Please select at least one pronoun.';
+		}
+
+		// Validate the custom verbs input field.
+		if (verbSettings.selectedVerbs === 'USER_DEFINED') {
+			if (verbSettings.userDefinedVerbs === '') {
+				// Check that the field has not been left blank.
+				formIsValid = false;
+				formErrors['custom-verbs'] = 'Please enter some verbs.';
+			} else if (verbSettings.validUserDefinedVerbs.length === 0) {
+				// Check that the verbs entered are actually valid.
+				formIsValid = false;
+				formErrors['custom-verbs'] =
+					'Please enter some valid verbs seperated by a comma.';
+			}
+		}
+
+		dispatch(setErrors(formErrors));
+		return formIsValid;
+	}
+
+	function setUpGame() {
+		setTimeout(() => {
+			// Generate a list of questions.
+			const questionList = generateQuestionList(
+				tenses,
+				pronouns,
+				verbSettings,
+				targetScore
+			);
+			dispatch(setQuestionList(questionList));
+
+			// Check that the number of questions matches the target score. With custom verbs, this may not be the case.
+			if (questionList.length < targetScore) {
+				// Set the target score to the length of available questions.
+				dispatch(setTargetScore(questionList.length));
+			}
+
+			// Setup the initial question.
+			const intialQuestion = questionList[0];
+			dispatch(
+				setCurrentQuestion(
+					1,
+					intialQuestion.verb,
+					intialQuestion.tense,
+					intialQuestion.pronoun
+				)
+			);
+
+			//Start the game. 
+			dispatch(toggleCurrentlyPlaying());
+			history.push('/game');
+		}, 1000);
+	}
 
 	return (
 		<div className="settings-column" id="settings-column">
 			<div className="column-header">Additional Settings</div>
 			<div className="validation-message">
-				{pronouns.every((pronoun) => !pronoun.selected) && (
-					<span>* Select at least one pronoun.</span>
-				)}
+				<span>{errors['pronouns']}</span>
 			</div>
 			<span className="section-header">Pronouns</span>
 			<div className="settings-section">
@@ -118,28 +194,19 @@ export default function VerbSettings() {
 						The following:
 					</label>
 					<div className="validation-message">
-						{verbSettings.selectedVerbs === 'USER_DEFINED' &&
-							verbSettings.userDefinedVerbs === '' && (
-								<span>* Enter some verbs.</span>
-							)}
+						<span>{errors['custom-verbs']}</span>
 					</div>
 					<input
 						id="custom-verbs"
 						value={verbSettings.userDefinedVerbs}
 						onChange={(evt) => dispatch(setUserDefinedVerbs(evt.target.value))}
-						placeholder="Enter some verbs to practice.."
+						placeholder="Enter some verbs followed by a comma. e.g. estar, ver, hablar"
 					></input>
 				</div>
 				<div className="button-section">
-					<button
-						type="submit"
-						id="btn-start"
-						disabled={tenseError || pronounsError || verbsError}>
+					<button type="submit" id="btn-start" onClick={handleSubmitClick}>
 						Start
 					</button>
-					{(tenseError || pronounsError || verbsError) && (
-						<div id="button-message">Please resolve errors to continue.</div>
-					)}
 				</div>
 			</div>
 		</div>
