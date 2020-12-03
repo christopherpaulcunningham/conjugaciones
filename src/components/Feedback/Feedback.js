d208e08b60639267a5d9ddb3dda6681b302cfa55
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2020-12-03 19:33:02 +0100

    Show score on finish

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { toggleCurrentlyPlaying } from '../../actions';
 import './Feedback.css';
 
-export default function Feedback() {	
+export default function Feedback() {
+	const score = useSelector((state) => state.score);
+	const targetScore = useSelector((state) => state.targetScore);
+	const dispatch = useDispatch();
+	const history = useHistory();
+
+	function handleClick() {
+		dispatch(toggleCurrentlyPlaying());
+		history.push('/');
+	}
 
 	return (
 		<div className="feedback-container">
-			
+			Your score is {score} out of {targetScore}
+			<button id="btn-finish" onClick={handleClick}>
+				Finish
+			</button>
 		</div>
 	);
 }
