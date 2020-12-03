91d929a3caffa88da6add232e23cacfdc4567830
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2020-12-03 19:24:16 +0100

    Protect routes from unauthorised access by the user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './App.css';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Header from './components/Header/Header';
-import Options from "./components/Options/Options";
-import Game from "./components/Game/Game";
+import Options from './components/Options/Options';
+import Game from './components/Game/Game';
+import Feedback from './components/Feedback/Feedback';
 import Footer from './components/Footer/Footer';
+
+function useIsCurrentlyPlaying() {
+	const currentlyPlaying = useSelector((state) => state.currentlyPlaying);
+	return currentlyPlaying;
+}
 
 function App() {
 	return (
@@ -12,7 +20,8 @@ function App() {
 				<Header />
 				<Switch>
 					<Route path="/" exact component={Options} />
-					<Route path="/game" component={Game} />
+					<PrivateRoute authenticated={useIsCurrentlyPlaying()} path="/game" component={Game}  />
+					<PrivateRoute authenticated={useIsCurrentlyPlaying()} path="/feedback" component={Feedback} />
 				</Switch>
 				<Footer />
 			</Router>
